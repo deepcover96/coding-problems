e6c341de1299455e7f30db28fdb66558f304ce2c
Wilfredo Ruiz <wil@Wilfredos-MacBook-Air.local>
2017-05-23 21:50:18 -0400

    Started Third

---
+++ Ch01_ArraysAndStrings/Three.java
@@ -0,0 +1,19 @@
+import java.util.*;  
+
+
+class Three {
+	public static void main(String[] args) {
+
+	}
+
+	// Write a method to replace all spaces in a string with '%20'.
+	// You may assume that the string has sufficient space at the 
+	// end to hold the additional characters, and that you are given
+	// the "true" length of the string.  Use a character array.
+	// Example:
+	//    Input:  "Mr John Smith    ", 13
+	//    Output: "Mr%20John%20Smith"
+	public static void URLify(char[] arr) {
+
+	}
+}
